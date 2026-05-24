3351b82dfcd4e135acd0deeb7949445b038115b8
unknown <2562687387@qq.com>
2026-05-24 22:17:55 +0800

    第一代模板

---
+++ src/main/java/org/fjnu305/acm01/Acm01Application.java
@@ -0,0 +1,13 @@
+package org.fjnu305.acm01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Acm01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Acm01Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/fjnu305/acm01/Acm01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fjnu305.acm01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Acm01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
